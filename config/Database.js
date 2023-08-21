7f9ed3dd7533a365e988aff4e75a2a7421c4e1a6
MSH1317 <125707401+MSH1317@users.noreply.github.com>
2023-08-21 21:58:38 +0700

    rollback

--- config/Database.js
+++ config/Database.js
@@ -3,7 +3,6 @@ import { Sequelize } from "sequelize";
 const db = new Sequelize('muhsyar11_auth','muhsyar11_auth','1234567890qwertyuiop', {
     host: "103.112.244.90",
     dialect: "mysql",
-    dialectModule: require('mysql2')
 })
 
 export default db;
