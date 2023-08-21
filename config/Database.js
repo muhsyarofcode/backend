10eece93a66d28966da3e33a32eee8f02ec8bb1e
MSH1317 <125707401+MSH1317@users.noreply.github.com>
2023-08-21 21:56:03 +0700

    last??!!

--- config/Database.js
+++ config/Database.js
@@ -2,7 +2,8 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize('muhsyar11_auth','muhsyar11_auth','1234567890qwertyuiop', {
     host: "103.112.244.90",
-    dialect: "mysql"
+    dialect: "mysql",
+    dialectModule: require('mysql2')
 })
 
 export default db;
