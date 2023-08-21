b37f6d432b745dbe51dabafdf7fdb303539ea1d4
MSH1317 <125707401+MSH1317@users.noreply.github.com>
2023-08-21 23:10:06 +0700

    aiwydahwd

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ dotenv.config();
 const app = express()
 
 
- 
+try {
+    await db.authenticate();
+    console.log('database connected');
+} catch (error) {
+    console.error(error)
+}   
+app.use(cors({credentials:true, origin:'http://localhost:3000'}))
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
