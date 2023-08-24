ee62fcd52d914b683991af9fe30a943f97815cad
MSH1317 <125707401+MSH1317@users.noreply.github.com>
2023-08-24 20:55:52 +0700

    JWT

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('muhsyar11_auth','muhsyar11_auth','1234567890qwertyuiop', {
-    host: "103.112.244.90",
-    dialect: "mysql",
+const db = new Sequelize('DB_NAME','root','', {
+    host: "localhost",
+    dialect: "mysql"
 })
 
 export default db;
